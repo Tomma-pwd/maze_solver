fbf083c5b0127f343623128d526da60dc540ccda
Tomma-pwd <babaski00@mail.ru>
2024-08-09 10:40:13 +0500

    destroy start and finish walls

--- src/maze.py
+++ src/maze.py
@@ -9,7 +9,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win = None,
     ):
         self._num_rows = num_rows
         self._num_cols = num_cols
@@ -20,6 +20,12 @@ class Maze:
         self._y1 = y1
         self._create_cells()
 
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
     def _create_cells(self):
         self._cells = [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]
         for i in range(self._num_cols):
@@ -27,6 +33,8 @@ class Maze:
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
+        if self._win is None:
+            return
         cell = self._cells[i][j]
         x1 = self._x1 + i * self._cell_size_x
         y1 = self._y1 + j * self._cell_size_y
@@ -39,4 +47,4 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.05)
+        time.sleep(0.01)
